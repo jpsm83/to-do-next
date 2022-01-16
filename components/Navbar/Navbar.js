89a826fe69d5e5aa893ac43ae511f9ac5e9a86e4
jpsm83 <jpsm83@hotmail.com>
2022-01-16 14:59:28 +0100

    header

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,41 +1,55 @@
+/* eslint-disable @next/next/link-passhref */
 import Link from "next/link";
 import { signOut, signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { BookOpenIcon, LoginIcon, LogoutIcon } from "@heroicons/react/outline";
 
 export default function Navbar() {
-  const router = useRouter();
   const { data: session } = useSession();
 
   return (
-    <nav className="p-20 mb-20 bg-blue-400 text-white flex justify-between">
-      <Link href="/">
-        <a>To Do App</a>
-      </Link>
-      {session ? (
-        <div>
-          <Link href="/createToDos">
-            <a>Create To Do</a>
-          </Link>
-          <div>
-            <p>{session.user.name}</p>
-            <div>
+    <nav>
+      <div className="p-5 bg-blue-400 text-gray-200 flex justify-between">
+        <Link href="/">
+          <div className="cursor-pointer flex space-x-2 items-center justify-start mr-6">
+            <BookOpenIcon className="h-10" />
+            <a>To Do App</a>
+          </div>
+        </Link>
+        {session ? (
+          <div className="flex space-x-4 items-center justify-between">
+            <p className="font-bold sm:text-xl text-md">Hello {session.user.name}</p>
+            <div
+              onClick={signOut}
+              className="cursor-pointer flex space-x-2 items-center"
+            >
               <Image
-                onClick={signOut}
                 src={session.user.image}
                 alt="User photo"
-                width={150}
-                height={40}
+                width={50}
+                height={50}
                 objectFit="contain"
-                className="cursor-pointer"
+                className="flex justify-center overflow-hidden items-center rounded-full"
               />
+              <LogoutIcon className="h-8" />
             </div>
           </div>
-        </div>
-      ) : (
-        <div>
-          <button onClick={signIn}>Sign In</button>
-        </div>
+        ) : (
+            <button onClick={signIn} className="flex space-x-2 items-center">
+            <p>Sign In</p>
+            <LoginIcon className="h-10" />
+            </button>
+        )}
+      </div>
+      {session && (
+        <div className="bg-gray-300 p-3 flex justify-center">
+        <Link href="/createToDos">
+          <button className="cursor-pointer shadow-md items-center pb-1 text-blue-700 text-center justify-center px-4 hover:bg-white font-bold hover:shadow-xl bg-gray-100 hover:scale-105 transition transform duration-200 ease-out active:scale-90 rounded-lg ">
+            Create To Do
+          </button>
+        </Link>
+      </div>
       )}
     </nav>
   );
