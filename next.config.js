b1b13e4f4e6149c60ebe5d0dbc7fa7119cc22611
jpsm83 <jpsm83@hotmail.com>
2022-01-18 17:34:28 +0100

    trailingSlash

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: false,
     // to use images with next/images you must allow the domain where those images comes from
     images: {
       domains: ["lh3.googleusercontent.com"],
