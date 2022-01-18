4921b6910bc8a640e4a9158148d9b5ccd023e212
jpsm83 <jpsm83@hotmail.com>
2022-01-18 13:18:12 +0100

    auth4

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,22 +2,27 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+  session: {
+    jwt: true,
+  },
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      // authorization: {
-      //   params: {
-      //     prompt: "consent",
-      //     access_type: "offline",
-      //     response_type: "code",
-      //   },
-      // },
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
-  // jwt: {
-  //   encryption: true,
-  // },
+  jwt: {
+    encryption: true,
+  },
   secret: process.env.SECRET,
+  // mongodb database or any other - no db, then leave empty
+  database: process.env.MONGO_URI
 });
