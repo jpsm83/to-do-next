748ac3fc3ad3fa2c96c8d7838114a2c6fa25ea0a
jpsm83 <jpsm83@hotmail.com>
2022-01-18 11:42:33 +0100

    favicon

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,9 +2,9 @@ import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/google";
 
 export default NextAuth({
-    // session: {
-    //     jwt: true
-    // },
+    session: {
+        jwt: true
+    },
   // Configure one or more authentication providers
   providers: [
     FacebookProvider({
